12f85f21b490a6aba99a27e6cadde4c16f7318b6
Muhammad Huzaifa Khan <57367781+HuzaifaKhan20@users.noreply.github.com>
2023-12-30 04:06:52 +0500

    Add files via upload

--- assets/js/practice-2.js
+++ assets/js/practice-2.js
@@ -54,6 +54,6 @@ else {
 }
 */
 
-let age = prompt("Enter your age");
-let a = age > 18 ? "You can drive" : "You cannot drive";
-console.log(a);
+// let age = prompt("Enter your age");
+// let a = age > 18 ? "You can drive" : "You cannot drive";
+// console.log(a);
