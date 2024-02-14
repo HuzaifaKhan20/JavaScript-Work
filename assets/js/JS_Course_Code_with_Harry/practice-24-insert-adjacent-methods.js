0c49c96ad15e5d17fc2b2ea52403b9ffdb0289c3
Muhammad Huzaifa Khan <57367781+HuzaifaKhan20@users.noreply.github.com>
2024-02-14 13:05:31 +0500

    Add files via upload

---
+++ assets/js/JS_Course_Code_with_Harry/practice-24-insert-adjacent-methods.js
@@ -0,0 +1,9 @@
+let first = document.getElementById("first");
+
+// first.insertAdjacentHTML('beforebegin', '<div class="beforebegin">beforebegin</div>')
+// first.insertAdjacentHTML('beforeend', '<div class="beforeend">beforeend</div>')
+// first.insertAdjacentHTML('afterbegin', '<div class="afterbegin">afterbegin</div>')
+// first.insertAdjacentHTML('afterend', '<div class="afterend">afterend</div>')
+
+
+first.remove();
---
+++ assets/js/JS_Course_Code_with_Harry/practice-25-change-className.js
@@ -0,0 +1,12 @@
+let first = document.getElementById("first");
+
+first.className = "text-black red";
+
+// first.classList.add("text-blue");
+
+// first.classList.remove("red");
+
+// first.classList.toggle("red");
+
+console.log(first.classList);
+console.log(first.classList.contains("red"));
